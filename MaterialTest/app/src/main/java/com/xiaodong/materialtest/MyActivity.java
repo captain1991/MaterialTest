d5ca58b45650dce917c15e89638b743edb67fa01
captain1991 <m18255031805_1@163.com>
2017-02-17 10:57:13 +0800

    Navitation添加

--- MaterialTest/app/src/main/java/com/xiaodong/materialtest/MyActivity.java
+++ MaterialTest/app/src/main/java/com/xiaodong/materialtest/MyActivity.java
@@ -1,18 +1,67 @@
 package com.xiaodong.materialtest;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by yxd on 2017/2/16.
  */
 public class MyActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_myactivity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        navigationView.setCheckedItem(R.id.call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+//                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.add:
+                Toast.makeText(this,"click add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.done:
+                Toast.makeText(this,"click done",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.write:
+                Toast.makeText(this,"click write",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
